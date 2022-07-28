7a6e7ad35936cb710ec4f29cfb1fdc6aed627e24
Harmanjot Kaur <109522011+Harmankjot18@users.noreply.github.com>
2022-07-28 20:03:43 +0530

    Constraint Layout

---
+++ app/src/main/java/com/harman/relative_layout/Constraint_Layout.kt
@@ -0,0 +1,11 @@
+package com.harman.relative_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
--- app/src/main/java/com/harman/relative_layout/MainActivity.kt
+++ app/src/main/java/com/harman/relative_layout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.harman.relative_layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.harman.relative_layout.databinding.ActivityMainBinding
@@ -8,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tvtraveller.setOnClickListener {
+            var intent = Intent(this, Constraint_Layout::class.java)
+            intent.putExtra("value",1)
+            startActivity(intent)
+        }
     }
 }
